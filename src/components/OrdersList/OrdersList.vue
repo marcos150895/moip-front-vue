9d48eb6770326ee3e82c66c52a941e1b7c8bc3d7
marcos150895 <marcos150895@gmail.com>
2017-05-16 14:25:29 -0300

    add shared paste and one component

--- src/components/OrdersList/OrdersList.vue
+++ src/components/OrdersList/OrdersList.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="container">
-    <h3>{{ msg }}</h3>
-    <hr>
+    <campo-valor :argumento1="msg" argumento2="Santana" icone="./assets/bar-codel.svg">
+    </campo-valor>
+     
+    <div id="linha">
+    </div>
+    
   </div>
 </template>
 
 <script>
+import CampoValor from '../shared/campoValor/CampoValor.vue';
+
 export default {
   name: 'orderslist',
+
+  components: {
+
+    'campo-valor': CampoValor
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '2345 Pedidos encontrados '
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 h1, h2 , h3, h4{
   font-weight: normal;
@@ -28,6 +40,14 @@ h1, h2 , h3, h4{
   margin-top: 5%;
   margin-left: auto;
 	margin-right: auto;
+}
+
+#linha {
+  border-bottom-color: white;
+  border-style: solid;
+  border-top: none;
+  border-left:none;
+  border-right: none;
 }
 
 </style>
---
+++ src/components/shared/campoValor/CampoValor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div id="texto"><img :src="icone">{{ argumento1 }} {{argumento2}} {{icone}} </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  
+    props: ['argumento1', 'argumento2', 'icone'],
+  
+    computed: {
+  
+    }
+  }
+</script>
+
+<style scoped>
+  h3 {
+    color: #0D47A1;
+  }
+  
+  #texto {
+    font-size: 1em;
+    margin-bottom: 1em;
+  }
+</style>
