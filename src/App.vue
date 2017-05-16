693efb6ea46161247f2c6d7f059ef46bca67dc62
Marcos Santana santo <marcos150895@gmail.com>
2017-05-15 21:04:12 -0300

    estrutura

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <orders-list></orders-list>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+
+import OrdersList from './components/OrdersList/OrdersList'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    OrdersList
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Quicksand;
+	font-size: 1.25em;
+	font-style: normal;
+	font-variant: normal;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+}
+
+body{
+    background: -webkit-linear-gradient(left, rgba(100,181,246,1), rgba(30,136,229,1)); /* For Safari 5.1 to 6.0 */
+    background: -o-linear-gradient(right, rgba(100,181,246,1), rgba(30,136,229,1)); /* For Opera 11.1 to 12.0 */
+    background: -moz-linear-gradient(right, rgba(100,181,246,1), rgba(30,136,229,1)); /* For Firefox 3.6 to 15 */
+    background: linear-gradient(to right, rgba(100,181,246,1), rgba(30,136,229,1)); /* Standard syntax (must be last) */
 }
 </style>
---
+++ src/components/OrdersList/OrdersList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h3>{{ msg }}</h3>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'orderslist',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 , h3, h4{
+  font-weight: normal;
+  text-align: center;
+}
+
+.container {
+  width: 80%;
+  margin-top: 5%;
+  margin-left: auto;
+	margin-right: auto;
+}
+
+</style>
